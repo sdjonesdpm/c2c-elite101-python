fbc8208d7d75960e57fe766f1d7a7f06cdddf6a2
sdjonesdpm <sdjonesdpm@icloud.com>
2025-11-10 22:58:30 +0000

    Edgecases

--- bot.py
+++ bot.py
@@ -2,10 +2,14 @@ def bot():
     print("Hi! My name is Helping Handy and you have entered Manny's Tex-Mex Eatery.")
 name = input("What is your name?")
 print(f"Nice to meet you {name},")
-age_input = input("How old are you?")
+age_input = input("People of a certain age recieve discounts!\n How old are you?")
 age = int(age_input)
 if age < 18:
-    print("You get 30% off your first order!")
+    print("You get 30% off your first order!\n")
+elif age >18:
+    print("Sorry, Looks like you don't qualify for a discount today.")
+else:
+    print("Please input a valid age.")
 
 print("How can I help you today?\n")
 
@@ -14,14 +18,21 @@ print("\n".join(options))
 number = input("Please choose from the following options:")
 if number == '5':
     print("Later! See you next time!")
+    exit()
 elif number == '1':
     print("We are open 24hrs a day, Monday through Saturday!")
 elif number == '2':
-    print("We are located at 512 Wigglesburg Lanes, Austin, TX 01410")
+    print("We are located at 512 Wigglesburg Ln, Austin, TX 01410")
 elif number == '3':
-    print("We have a variety of Tex-Mex foods\n")
-    products = ['Tacos', 'Burritos', 'Quesadillas','Sharable Nachos', 'Fajitas', 'Chips & Salsa']
-    print("/n".join(products))
+    print("We have a variety of Tex-Mex foods:\n")
+    products = ['Tacos\n', 'Burritos\n', 'Quesadillas\n','Sharable Nachos\n', 'Fajitas\n', 'Chips & Salsa\n']
+    for item in products:
+        print(item)
 elif number == '4':
     print("Our prices have been affordable since 1738!/n Here is a list of our prices per item:")
 prices = ['Tacos: 3.50 each', 'Burritos: 4.50 each', 'Quesadillas: 2.00 each', 'Sharable Nachos: 5.00 each', 'Fajitas: 6.50 each', 'Chips & Salsa: 1.50 each']
+for price in prices:
+    print(price)
+def goodbye():
+    print("Do you need anything else today?")
+    return bye_input = input("Please enter Y for Yes")
