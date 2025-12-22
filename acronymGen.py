1e00ec6986505f60c507cb82db41b29ce331e3c7
sdjonesdpm <sdjonesdpm@icloud.com>
2025-12-22 23:13:01 +0000

    1st attempt

---
+++ acronymGen.py
@@ -0,0 +1,8 @@
+def acronym_generator():
+# Prompt the user to enter a phrase
+    prompt = input("Please enter a phrase and I'll generate its acronym:")
+#Spilt the phrase into words(ignore blank segments or punctuactions)
+    words = [word for word in prompt.split() if word.isalnum()]
+#Collect the first letter of each word, uppercase it, and join into the final acronym
+    acronym = ''.join(word[0].upper()for word in words)
+
